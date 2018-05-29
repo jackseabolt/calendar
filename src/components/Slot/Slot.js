84836a37b4402f7b6385c17fc7e4210cd4d97a53
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-05-29 07:04:07 -0700

    Slot component cleaned

--- src/components/Slot/Slot.js
+++ src/components/Slot/Slot.js
@@ -1,10 +1,17 @@
 import React from 'react'; 
-import './Slot.css'; 
-import { setSelectedTime, toggleModal, setModalContent, setSelectedTitle, setSelectedType } from '../../actions/main'; 
-import { connect } from 'react-redux'; 
 import Event from '../Event'; 
+import './Slot.css'; 
+import { 
+    setSelectedTime, 
+    toggleModal, 
+    setModalContent, 
+    setSelectedTitle, 
+    setSelectedType 
+} from '../../actions/main'; 
+import { connect } from 'react-redux'; 
 
 export class Slot extends React.Component {
+    
     handleClick() {
         this.props.dispatch(setSelectedTime(this.props.slot.time));
         if(this.props.slot.event) {
@@ -15,25 +22,33 @@ export class Slot extends React.Component {
             this.props.dispatch(setSelectedTitle(null)); 
             this.props.dispatch(setModalContent('new')) 
         }
-        
         this.props.dispatch(toggleModal());
     }
     
     render() {
+        // controls slot weekend vs. week styles
         let slotStyle; 
         if(this.props.day === "Saturday" || this.props.day === "Sunday") {
-            slotStyle = "Slot-weekend"
+            slotStyle = "Slot-weekend";
         } 
         else {
-            slotStyle = "Slot"
+            slotStyle = "Slot";
         }
 
-        let contents = this.props.slot.event ? <Event event={this.props.slot.event}/> : <p className="time">{this.props.slot.time}</p>; 
+        // display Event if present
+        let contents; 
+        if(this.props.slot.event) {
+            contents = <Event event={this.props.slot.event}/>
+        } 
+        else {
+            contents = <p className="time">{this.props.slot.time}</p>;
+        } 
+        
         return (
             <section onClick={() => this.handleClick() } className={slotStyle}>
                 {contents}   
             </section>
-        )
+        ); 
     }
 }
 
